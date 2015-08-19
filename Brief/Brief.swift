d365c20478f077c9fdb787766f7b2f330a490797
kakajika <kakajika200x@gmail.com>
2015-08-20 07:57:48 +0900

    Remove useless properties in Swift Make properties access more efficiently Fix left/top/right/bottom

--- Brief/Brief.swift
+++ Brief/Brief.swift
@@ -9,255 +9,140 @@ import UIKit
 
 extension UIView {
     
-    // Frame
-    public var frameOrigin: CGPoint {
-        get { return self.frame.origin }
-        set(frameOrigin) { self.frame = CGRect(origin: frameOrigin, size: frameSize) }
-    }
-    public var frameSize: CGSize {
-        get { return self.frame.size }
-        set(frameSize) { self.frame = CGRect(origin: frameOrigin, size: frameSize) }
-    }
-    
     // Frame Origin
     public var x: CGFloat {
-        get { return self.frame.minX }
-        set(x) { self.frame = CGRectMake(x, y, width, height) }
+        get { return self.frame.origin.x }
+        set(x) { self.frame.origin.x = x }
     }
     public var y: CGFloat {
-        get { return self.frame.minY }
-        set(y) { self.frame = CGRectMake(x, y, width, height) }
+        get { return self.frame.origin.y }
+        set(y) { self.frame.origin.y = y }
     }
     
     // Frame Size
     public var width: CGFloat {
         get { return self.frame.width }
-        set(width) { self.frame = CGRectMake(x, y, width, height) }
+        set(width) { self.frame.size.width = width }
     }
     public var height: CGFloat {
         get { return self.frame.height }
-        set(height) { self.frame = CGRectMake(x, y, width, height) }
+        set(height) { self.frame.size.height = height }
     }
     
     // Frame Borders
     public var left: CGFloat {
         get { return self.frame.minX }
-        set(left) { self.frame = CGRectMake(left, top, width, height) }
+        set(left) { self.frame.origin.x = left - min(width, 0.0) }
     }
     public var top: CGFloat {
         get { return self.frame.minY }
-        set(top) { self.frame = CGRectMake(left, top, width, height) }
+        set(top) { self.frame.origin.y = top - min(height, 0.0) }
     }
     public var right: CGFloat {
         get { return self.frame.maxX }
-        set(right) { self.frame = CGRectMake(right-width, top, width, height) }
+        set(right) { self.frame.origin.x = right - max(width, 0.0) }
     }
     public var bottom: CGFloat {
         get { return self.frame.maxY }
-        set(bottom) { self.frame = CGRectMake(left, bottom-height, width, height) }
+        set(bottom) { self.frame.origin.y = bottom - max(width, 0.0) }
     }
     
     // Bounds
-    public var boundsOrigin: CGPoint {
-        get { return self.bounds.origin }
-        set(boundsOrigin) { self.bounds = CGRect(origin: boundsOrigin, size: boundsSize) }
-    }
-    public var boundsSize: CGSize {
-        get { return self.bounds.size }
-        set(boundsSize) { self.bounds = CGRect(origin: boundsOrigin, size: boundsSize) }
-    }
     public var boundsWidth: CGFloat {
         get { return self.bounds.width }
-        set(boundsWidth) { self.bounds = CGRectMake(boundsLeft, boundsTop, boundsWidth, boundsHeight) }
+        set(boundsWidth) { self.bounds.size.width = boundsWidth }
     }
     public var boundsHeight: CGFloat {
         get { return self.bounds.height }
-        set(boundsHeight) { self.bounds = CGRectMake(boundsLeft, boundsTop, boundsWidth, boundsHeight) }
+        set(boundsHeight) { self.bounds.size.height = boundsHeight }
     }
+    
+    // Bounds Borders
     public var boundsLeft: CGFloat {
         get { return self.bounds.minX }
-        set(boundsLeft) { self.bounds = CGRectMake(boundsLeft, boundsTop, boundsWidth, boundsHeight) }
+        set(boundsLeft) { self.bounds.origin.x = boundsLeft - min(boundsWidth, 0.0) }
     }
     public var boundsTop: CGFloat {
         get { return self.bounds.minY }
-        set(boundsTop) { self.bounds = CGRectMake(boundsLeft, boundsTop, boundsWidth, boundsHeight) }
+        set(boundsTop) { self.bounds.origin.y = boundsTop - min(boundsHeight, 0.0) }
     }
     public var boundsRight: CGFloat {
         get { return self.bounds.maxX }
-        set(boundsRight) { self.bounds = CGRectMake(boundsRight-boundsWidth, boundsTop, boundsWidth, boundsHeight) }
+        set(boundsRight) { self.bounds.origin.x = boundsRight - max(boundsWidth, 0.0) }
     }
     public var boundsBottom: CGFloat {
         get { return self.bounds.maxY }
-        set(boundsBottom) { self.bounds = CGRectMake(boundsLeft, boundsBottom-boundsHeight, boundsWidth, boundsHeight) }
-    }
-    
-    // Center Point
-    public var centerX: CGFloat {
-        get { return self.center.x }
-        set(centerX) { self.center = CGPointMake(centerX, centerY) }
-    }
-    public var centerY: CGFloat {
-        get { return self.center.y }
-        set(centerY) { self.center = CGPointMake(centerX, centerY) }
-    }
-    
-    // Middle Point
-    public var middlePoint: CGPoint {
-        get { return CGPointMake(middleX, middleY) }
-    }
-    public var middleX: CGFloat {
-        get { return width * 0.5 }
-    }
-    public var middleY: CGFloat {
-        get { return height * 0.5 }
-    }
-    
-}
-
-extension UIScrollView {
-    
-    // Content Offset
-    public var contentOffsetX: CGFloat {
-        get { return self.contentOffset.x }
-        set(contentOffsetX) { self.contentOffset = CGPointMake(contentOffsetX, contentOffsetY) }
-    }
-    public var contentOffsetY: CGFloat {
-        get { return self.contentOffset.y }
-        set(contentOffsetY) { self.contentOffset = CGPointMake(contentOffsetX, contentOffsetY) }
-    }
-    
-    // Content Size
-    public var contentSizeWidth: CGFloat {
-        get { return self.contentSize.width }
-        set(contentSizeWidth) { self.contentSize = CGSizeMake(contentSizeWidth, contentSizeHeight) }
-    }
-    public var contentSizeHeight: CGFloat {
-        get { return self.contentSize.height }
-        set(contentSizeHeight) { self.contentSize = CGSizeMake(contentSizeWidth, contentSizeHeight) }
-    }
-    
-    // Content Inset
-    public var contentInsetTop: CGFloat {
-        get { return self.contentInset.top }
-        set(contentInsetTop) { self.contentInset = UIEdgeInsetsMake(contentInsetTop, contentInsetLeft, contentInsetBottom, contentInsetRight) }
-    }
-    public var contentInsetLeft: CGFloat {
-        get { return self.contentInset.left }
-        set(contentInsetLeft) { self.contentInset = UIEdgeInsetsMake(contentInsetTop, contentInsetLeft, contentInsetBottom, contentInsetRight) }
-    }
-    public var contentInsetBottom: CGFloat {
-        get { return self.contentInset.bottom }
-        set(contentInsetBottom) { self.contentInset = UIEdgeInsetsMake(contentInsetTop, contentInsetLeft, contentInsetBottom, contentInsetRight) }
-    }
-    public var contentInsetRight: CGFloat {
-        get { return self.contentInset.right }
-        set(contentInsetRight) { self.contentInset = UIEdgeInsetsMake(contentInsetTop, contentInsetLeft, contentInsetBottom, contentInsetRight) }
+        set(boundsBottom) { self.bounds.origin.y = boundsBottom - max(boundsHeight, 0.0) }
     }
     
 }
 
 extension CALayer {
     
-    // Frame
-    public var frameOrigin: CGPoint {
-        get { return self.frame.origin }
-        set(frameOrigin) { self.frame = CGRect(origin: frameOrigin, size: frameSize) }
-    }
-    public var frameSize: CGSize {
-        get { return self.frame.size }
-        set(frameSize) { self.frame = CGRect(origin: frameOrigin, size: frameSize) }
-    }
-    
     // Frame Origin
     public var x: CGFloat {
-        get { return self.frame.minX }
-        set(x) { self.frame = CGRectMake(x, y, width, height) }
+        get { return self.frame.origin.x }
+        set(x) { self.frame.origin.x = x }
     }
     public var y: CGFloat {
-        get { return self.frame.minY }
-        set(y) { self.frame = CGRectMake(x, y, width, height) }
+        get { return self.frame.origin.y }
+        set(y) { self.frame.origin.y = y }
     }
     
     // Frame Size
     public var width: CGFloat {
         get { return self.frame.width }
-        set(width) { self.frame = CGRectMake(x, y, width, height) }
+        set(width) { self.frame.size.width = width }
     }
     public var height: CGFloat {
         get { return self.frame.height }
-        set(height) { self.frame = CGRectMake(x, y, width, height) }
+        set(height) { self.frame.size.height = height }
     }
     
     // Frame Borders
     public var left: CGFloat {
         get { return self.frame.minX }
-        set(left) { self.frame = CGRectMake(left, top, width, height) }
+        set(left) { self.frame.origin.x = left - min(width, 0.0) }
     }
     public var top: CGFloat {
         get { return self.frame.minY }
-        set(top) { self.frame = CGRectMake(left, top, width, height) }
+        set(top) { self.frame.origin.y = top - min(height, 0.0) }
     }
     public var right: CGFloat {
         get { return self.frame.maxX }
-        set(right) { self.frame = CGRectMake(right-width, top, width, height) }
+        set(right) { self.frame.origin.x = right - max(width, 0.0) }
     }
     public var bottom: CGFloat {
         get { return self.frame.maxY }
-        set(bottom) { self.frame = CGRectMake(left, bottom-height, width, height) }
+        set(bottom) { self.frame.origin.y = bottom - max(width, 0.0) }
     }
     
     // Bounds
-    public var boundsOrigin: CGPoint {
-        get { return self.bounds.origin }
-        set(boundsOrigin) { self.bounds = CGRect(origin: boundsOrigin, size: boundsSize) }
-    }
-    public var boundsSize: CGSize {
-        get { return self.bounds.size }
-        set(boundsSize) { self.bounds = CGRect(origin: boundsOrigin, size: boundsSize) }
-    }
     public var boundsWidth: CGFloat {
         get { return self.bounds.width }
-        set(boundsWidth) { self.bounds = CGRectMake(boundsLeft, boundsTop, boundsWidth, boundsHeight) }
+        set(boundsWidth) { self.bounds.size.width = boundsWidth }
     }
     public var boundsHeight: CGFloat {
         get { return self.bounds.height }
-        set(boundsHeight) { self.bounds = CGRectMake(boundsLeft, boundsTop, boundsWidth, boundsHeight) }
+        set(boundsHeight) { self.bounds.size.height = boundsHeight }
     }
+    
+    // Bounds Borders
     public var boundsLeft: CGFloat {
         get { return self.bounds.minX }
-        set(boundsLeft) { self.bounds = CGRectMake(boundsLeft, boundsTop, boundsWidth, boundsHeight) }
+        set(boundsLeft) { self.bounds.origin.x = boundsLeft - min(boundsWidth, 0.0) }
     }
     public var boundsTop: CGFloat {
         get { return self.bounds.minY }
-        set(boundsTop) { self.bounds = CGRectMake(boundsLeft, boundsTop, boundsWidth, boundsHeight) }
+        set(boundsTop) { self.bounds.origin.y = boundsTop - min(boundsHeight, 0.0) }
     }
     public var boundsRight: CGFloat {
         get { return self.bounds.maxX }
-        set(boundsRight) { self.bounds = CGRectMake(boundsRight-boundsWidth, boundsTop, boundsWidth, boundsHeight) }
+        set(boundsRight) { self.bounds.origin.x = boundsRight - max(boundsWidth, 0.0) }
     }
     public var boundsBottom: CGFloat {
         get { return self.bounds.maxY }
-        set(boundsBottom) { self.bounds = CGRectMake(boundsLeft, boundsBottom-boundsHeight, boundsWidth, boundsHeight) }
-    }
-    
-    // Anchor Point
-    public var anchorX: CGFloat {
-        get { return self.anchorPoint.x }
-        set(anchorX) { self.anchorPoint = CGPointMake(anchorX, anchorY) }
-    }
-    public var anchorY: CGFloat {
-        get { return self.anchorPoint.y }
-        set(anchorY) { self.anchorPoint = CGPointMake(anchorX, anchorY) }
-    }
-    
-    // Position
-    public var positionX: CGFloat {
-        get { return self.position.x }
-        set(positionX) { self.position = CGPointMake(positionX, positionY) }
-    }
-    public var positionY: CGFloat {
-        get { return self.position.y }
-        set(positionY) { self.position = CGPointMake(positionX, positionY) }
+        set(boundsBottom) { self.bounds.origin.y = boundsBottom - max(boundsHeight, 0.0) }
     }
     
 }
--- README.md
+++ README.md
@@ -20,12 +20,14 @@ Just add [Brief.swift](Brief/Brief.swift) into your project.
 ## Usage
 ```
 // Before
-view.frame = CGRectMake(20.0, view.frame.minY, view.frame.width, view.frame.height)
-view.frame = CGRectMake(view.frame.minX, view.frame.minY, 200.0, view.frame.height)
+view.frame.origin.x = 20.0
+view.frame.size.width = 200.0
+view.frame.origin.y = 100.0 - view.frame.size.height // Make view's bottom to 100.0
 
 // After 
 view.x = 20.0
 view.width = 200.0
+view.bottom = 100.0
 ```
 
 ## Supported Properties
@@ -34,42 +36,36 @@ view.width = 200.0
 
 Property | Type | Аvailability
 --- | --- | ---
-`frameOrigin` | `CGPoint` | *get/set*
-`frameSize` | `CGSize` | *get/set*
 `x`, `y` | `CGFloat` | *get/set*
 `width`, `height` | `CGFloat` | *get/set*
 `top`, `left`, `bottom`, `right` | `CGFloat` | *get/set*
-`boundsOrigin` | `CGPoint` | *get/set*
-`boundsSize` | `CGSize` | *get/set*
 `boundsWidth`, `boundsHeight` | `CGFloat` | *get/set*
 `boundsTop`, `boundsLeft`, `boundsBottom`, `boundsRight` | `CGFloat` | *get/set*
-`centerX`, `centerY` | `CGFloat` | *get/set*
-`middlePoint` | `CGPoint` | **get**
-`middleX`, `middleY` | `CGFloat` | **get**
-
-`UIScrollView` properties:
-
-Property | Type | Аvailability
---- | --- | ---
-`contentOffsetX`, `contentOffsetY` | `CGFloat` | *get/set*
-`contentSizeWidth`, `contentSizeHeight` | `CGFloat` | *get/set*
-`contentInsetTop`, `contentInsetLeft`, <br>`contentInsetBottom`, `contentInsetRight` | `CGFloat` | *get/set*
 
 `CALayer` properties:
 
 Property | Type | Аvailability
 --- | --- | ---
-`frameOrigin` | `CGPoint` | *get/set*
-`frameSize` | `CGSize` | *get/set*
 `x`, `y` | `CGFloat` | *get/set*
 `width`, `height` | `CGFloat` | *get/set*
 `top`, `left`, `bottom`, `right` | `CGFloat` | *get/set*
-`boundsOrigin` | `CGPoint` | *get/set*
-`boundsSize` | `CGSize` | *get/set*
 `boundsWidth`, `boundsHeight` | `CGFloat` | *get/set*
 `boundsTop`, `boundsLeft`, `boundsBottom`, `boundsRight` | `CGFloat` | *get/set*
-`anchorX`, `anchorY` | `CGFloat` | *get/set*
-`positionX`, `positionY` | `CGFloat` | *get/set*
+
+### Deprecated
+
+Following properties are useless in Swift and removed.
+
+- `UIView`
+-- `frameOrigin`, `frameSize`
+-- `centerX`, `centerY`
+-- `middlePoint`, `middleX`, `middleY`
+- `UIScrollView`
+-- All properties
+- `CALayer`
+-- `frameOrigin`, `frameSize`
+-- `anchorX`, `anchorY`
+-- `positionX`, `positionY`
 
 ## ToDo
 - Support for OSX
